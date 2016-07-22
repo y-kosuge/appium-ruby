2831a23f1296ced91073ce0f490743d2a4981f36
y-kosuge <y-kosuge@oz-vision.co.jp>
2016-07-22 17:17:04 +0900

    Use env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,50 +1,50 @@
 require "rubygems"
 require "appium_lib"
 
-# iOS
 def desired_caps
-  {
-    caps: {
-      browserName: '',
-      #'appium-version': '1.3',
-      platformName: 'iOS',
-      platformVersion: '9.2',
-      deviceName: 'iPhone 5s',
-      app: 'platforms/ios/build/emulator/HelloCordova.app',
-      autoWebview: true,
-      autoWebViewTimeout: 10000
-    },
-    appium_lib: {
-      wait: 10
+  test_device = ENV['TEST_DEVICE']
+  case test_device
+  when 'ios'
+    {
+      caps: {
+        browserName: '',
+        'appium-version': '1.3',
+        platformName: 'iOS',
+        platformVersion: '9.2',
+        deviceName: 'iPhone 5s',
+        app: 'platforms/ios/build/emulator/HelloCordova.app',
+        autoWebview: true,
+        autoWebViewTimeout: 10000
+      },
+      appium_lib: {
+        wait: 10
+      }
     }
-  }
-end
-
-=begin
-# Android
-def desired_caps
-  {
-    caps: {
-      platformName:  "Android",
-      versionNumber: "4.1",
-      deviceName:    "Android Emulator",
-      avd: 'Nexus_5_API_21_x86',
-      app: 'platforms/android/build/outputs/apk/android-debug.apk',
-      :"app-activity" => '.MainActivity',
-      autoWebview: true,
-      autoWebViewTimeout: 10000
-    },
-    appium_lib: {
-      wait: 10
+  when 'android'
+    {
+      caps: {
+        platformName:  "Android",
+        versionNumber: "4.1",
+        deviceName:    "Android Emulator",
+        avd: 'Nexus_5_API_21_x86',
+        app: 'platforms/android/build/outputs/apk/android-debug.apk',
+        :"app-activity" => '.MainActivity',
+        autoWebview: true,
+        autoWebViewTimeout: 10000
+      },
+      appium_lib: {
+        wait: 10
+      }
     }
-  }
+  else
+    raise "環境変数 TEST_DEVICE に'android'または'ios'を設定してください"
+  end
 end
-=end
 
 RSpec.configure { |c|
   c.before(:each) {
     @driver = Appium::Driver.new(desired_caps).start_driver
-    @driver.manage.timeouts.implicit_wait = 5
+    @driver.manage.timeouts.implicit_wait = 10
     Appium.promote_appium_methods Object
   }
 
